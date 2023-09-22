4401877debadb8c0d3cf4c46a94f63f3acaea518
Za Matheson <zamatheson@Zas-MacBook-Air.lan>
2023-09-22 17:54:01 -0500

    worked on functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +10,49 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
+function validateLength(passLength) {
+  var passLength = Number(prompt("Enter a password length of 8-128 characters."));
+  console.log(passLength);
+
+  if (validateLength(passLength)) {
+  } else {
+    alert("The password you have entered is invalid.")
+    return '';
+  }
+
+}
+
+
+function validateCriteria() {
+   var charCriteria = prompt("Please include: uppercase, lowercase, number and special");
+  console.log(charCriteria);
+
+  if (validateCriteria(charCriteria)) {
+  } else {
+    alert("The password you have entered is invalid")
+    return '';
+  }
+
+}
+
+
+
+
+function generatePassword() {
+var charArray = { 
+  uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lowercase = 'abcdefghijklmnopqrstuvwxyz',
+  number = '0123456789',
+  special = '!@#$%^&*()_+{}|:"<>?/.,';[]'\',
+
+}
+
+function getRandomInt(max: any) {
+  return Math.floor(Math.random() * max: any);
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
